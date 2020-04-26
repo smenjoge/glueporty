a3b80e539df799201fc37be193e39298e5198d22
Sandesh Menjoge <smenjoge@gmail.com>
2020-04-26 11:53:38 -0400

    fixed issue with getting data for correct date from local storage

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # glueporty
 Day Planner Homework (wk5)
+1) Using provided html and CSS files, added new div inside the container class for one timeslot to display on the page.
+2) Bootstrap class "input-group" is used to display timeslot value, textarea and Save button.
+3) In script.js, the first fucntion to execute is to get current day and date value and display it in the p tag with id = "currentDay"
+4) Then all timeslots for a given workday (9am to 5pm) are displayed. To do this, the the html element with class 'time-block' is cloned and modified as needed.
+5) If a time slot is already passed for the current day, the textarea and save button are disabled to prevent user from changing the values. 
+6) If user has already set a reminder/meeting/thingToDo for a particular timeslot, its value is fetched from the local storage and displayed in that timeslot textarea. 
+7) When user enters/changes the value in a textarea, the value is stored in local storage in the form of an object. Two properties in the object represent the hour value and the text value. 
--- script.js
+++ script.js
@@ -7,29 +7,32 @@ $(document).ready(function() {
             scheduleArr: []
     };
     
-
+    // Function to display curret date and Day in the page header
     function getCurrentDayDate () {
         currentDate = moment().format('YYYY-MM-DD');
         currentDayDate = moment().format('dddd, MMMM Do YYYY');
         return currentDayDate;
     };
 
+    //Function to load all time slots on page load. 
     function loadDaySlots () {
-        var curentHour = moment().hour();
-        var dayScheduleStorage = JSON.parse(localStorage.getItem("daySchedule"));
+        var curentHour = moment().hour(); //get the value of current hour
         
+        //Get the schedule data stored in local storage. If no data found, initialize the object 
+        var dayScheduleStorage = JSON.parse(localStorage.getItem("daySchedule"));
         if (!dayScheduleStorage) {
             dayScheduleStorage = daySchedule;
         }
 
-
+        //Load the time slots from 9am to 5pm on page.  
         for(i=9; i < 18; i++) {
             if (firstIternation) {
-                var newTimeBlock = $(".time-block:last");
+                var newTimeBlock = $(".time-block"); //For loading first time slot, use the existing HTML element
                 firstIternation = false;
             }
             else {
-                var newTimeBlock = $(".time-block:last").clone();
+                //For loading all subsequent slots, clone the existing HTML element
+                var newTimeBlock = $(".time-block:last").clone(); 
             }
 
             var hourEl = newTimeBlock.find(".hour");
@@ -37,10 +40,10 @@ $(document).ready(function() {
             hourEl.text(moment(i, "h").format("hh:mm A"));
             
             var textAreaEl = newTimeBlock.find("textarea");
-            textAreaEl.attr("disabled", false);
+            textAreaEl.attr("disabled", false);//reset the disabled attribute
 
             var buttonEl = newTimeBlock.find(".saveBtn");
-            buttonEl.attr("disabled", false);
+            buttonEl.attr("disabled", false);//reset the disabled attribute
 
             if (curentHour === i) {
                 textAreaEl.addClass("present");
@@ -51,12 +54,15 @@ $(document).ready(function() {
             }
 
             else if (curentHour > i) {
-                textAreaEl.attr("disabled", true);
-                buttonEl.attr("disabled", true);
+                textAreaEl.attr("disabled", true);//For past timeslots disable textarea
+                buttonEl.attr("disabled", true);//For past timeslots disable Save button
                 textAreaEl.addClass("past");
             }
-            // find and populate text
-            textAreaEl.empty();
+            
+            textAreaEl.empty(); //empty the textarea field in case it has any value from previous cloned element
+            
+            // find if any data is saved in local storage for the current timeslot. If found, get the 
+            // value from local storage and display on the page
             if (dayScheduleStorage.dateObj == currentDate) {
                 findTimeSlot = dayScheduleStorage.scheduleArr.find(({timeObj} ) => timeObj == i);
                 if (findTimeSlot)  {
@@ -69,16 +75,16 @@ $(document).ready(function() {
     };
 
     function saveEvent () {
+        //Get the schedule data stored in local storage. If no data found, initialize the object 
         var dayScheduleStorage = JSON.parse(localStorage.getItem("daySchedule"));
-        
-        if (!dayScheduleStorage) {
+        if (!dayScheduleStorage || dayScheduleStorage.dateObj != currentDate ) {
             daySchedule.dateObj = currentDate;
             dayScheduleStorage = daySchedule;
         }
 
-        var parentElModify = $(this).parents(".time-block");
-        var hourModify = parentElModify.find(".hour").attr("value");
-        var toDoModify = parentElModify.find(".toDoClass").val();
+        var parentElModify = $(this).parents(".time-block"); //get reference to the time-block that is modified
+        var hourModify = parentElModify.find(".hour").attr("value");//check which hour time-block is modified
+        var toDoModify = parentElModify.find(".toDoClass").val();//get the new value entered in the textarea 
 
         var newtimeEventObj = {
             timeObj: hourModify,
@@ -88,6 +94,7 @@ $(document).ready(function() {
         // find if the event already exists for the timeslot that user clicked on
         var findscheduleObj = dayScheduleStorage.scheduleArr.find(({timeObj} ) => timeObj == hourModify);
 
+        //If found, replace the array element with new value. if not found, push a new element to the array 
         if (findscheduleObj) {
             var indexofScheduleObj = dayScheduleStorage.scheduleArr.indexOf(findscheduleObj);
             dayScheduleStorage.scheduleArr.splice(indexofScheduleObj, 1, newtimeEventObj);
